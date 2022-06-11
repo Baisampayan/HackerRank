09001f2fed79fdfe7b9eb66c92af123eae7c3c17
Baisampayan <baisampayandas@gmail.com>
2022-06-11 20:35:18 +0530

    Added: Hacker Rank in String

--- HackerRank in a String/solution.js
+++ HackerRank in a String/solution.js
@@ -1,36 +1,16 @@
+// Write your code here
 let i = 0;
-let s = "www.abc.xy";
-let k = 10;
-let newStr = "";
-
-if(k>26) {
-    k = k%26;
-    console.log(k);
-}
-
-for(i=0; i<s.length; i++) {
-    let asciiVal = s.charCodeAt(i) + k;
-    if(s.charCodeAt(i)>=97 && s.charCodeAt(i)<=122) {
-        // Getting Lower Case
-        if(asciiVal>122) {
-            asciiVal = 96 + (asciiVal - 122);
-            newStr += String.fromCharCode(asciiVal);
-        } else {
-            newStr += String.fromCharCode(asciiVal);
-        }
-    } else if (s.charCodeAt(i)>=65 && s.charCodeAt(i)<=90) {
-        // Getting Upper Case
-        if(asciiVal>90) {
-            asciiVal = 64 + (asciiVal - 90);
-            newStr += String.fromCharCode(asciiVal);
-            console.log(`ASCII Index: ${asciiVal} | ASCII Value: ${String.fromCharCode(asciiVal)}`);
-        } else {
-            newStr += String.fromCharCode(asciiVal);
-        }
-    } else {
-        // Getting non-alphabets
-        newStr += s[i];
+let j = 0;
+let s = "hhaacckkekraraannk";
+let inputStr = "hackerrank";
+while(i != s.length) {
+    if(s[i]==inputStr[j]) {
+        j += 1;
     }
+    i += 1;
 }
-
-console.log(newStr);
+if(inputStr.length=j) {
+    console.log("YES");
+} else {
+    console.log("NO");
+}
